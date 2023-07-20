547737d1143013a393635a094b9a56df65f9ad1f
eduardorgv <eduardsabb@outlook.com>
2023-07-20 08:17:22 -0600

    Componente para calificar

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// import { StarRating } from './movie/StarRating.jsx'
+
+// const Test = () => {
+//   const [movieRating, setMovieRating] = useState(0);
+
+//   return (
+//     <div>
+//       <StarRating color='blue' maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating}</p>
+//     </div>
+//   )
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
